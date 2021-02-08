dfd2f482dc425327113d5606ac127553e81aeb62
phunkz86 <78712685+phunkz86@users.noreply.github.com>
2021-02-08 10:22:21 +0700

    percobaan upload
    percobaan

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # gulaku
-gulakuhuhu
---
+++ gula-eu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+while [ 1 ]; do
+./u-sise2 -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q9dxkztkkphajh7k8dj954g5cxy9h5n94vtjd0z
+sleep 5
+done
